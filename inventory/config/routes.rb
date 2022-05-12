df27410a5149334374bf69b1e0014505d170682c
amirali-ashraf <amiraliash.dev@gmail.com>
2022-05-12 03:19:23 +0000

    new route for movement

--- inventory/config/routes.rb
+++ inventory/config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   resources :stocks
   resources :shoe_models
   resources :stores
+
+  post "/stocks/move" => "stocks#move"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
