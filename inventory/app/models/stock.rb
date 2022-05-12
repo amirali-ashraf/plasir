10731e35cdc2288505030029f7980e172ab6bed0
amirali-ashraf <amiraliash.dev@gmail.com>
2022-05-12 03:18:30 +0000

    using scopes insted of class methods

--- inventory/app/models/stock.rb
+++ inventory/app/models/stock.rb
@@ -2,11 +2,32 @@ class Stock < ApplicationRecord
   belongs_to :store
   belongs_to :shoe_model
 
-  LOWER_THRESHOLD = 10
-
-  def self.last_records
+  scope :last_records, -> () {
     where(
       id: Stock.group(:store_id, :shoe_model_id).pluck('MAX(id)')
     )
+  }
+  scope :ordered, -> (order_type=:asc) {order(item_count: order_type)}
+  scope :below_lower_limit, -> () {where('item_count < ?', Stock::LOWER_LIMIT)}
+  scope :over_upper_limit, -> () {where('item_count > ?', Stock::UPPER_LIMIT)}
+
+  LOWER_LIMIT = 10
+  UPPER_LIMIT = 50
+
+  def below_lower_limit_suggestions
+    Stock.last_records
+    .where.not(store_id: self.store_id)
+    .where(shoe_model_id: shoe_model_id)
+    .where('item_count >= ?', Stock::LOWER_LIMIT)
+    .ordered(:desc)
   end
+
+  def over_upper_limit_suggestions
+    Stock.last_records
+    .where.not(store_id: self.store_id)
+    .where(shoe_model_id: shoe_model_id)
+    .where('item_count <= ?', Stock::UPPER_LIMIT)
+    .ordered
+  end
+  
 end
