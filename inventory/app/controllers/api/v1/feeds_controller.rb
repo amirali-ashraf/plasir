eb332d85eeeb1b6b9092252f7fa792689f884e91
amirali-ashraf <amiraliash.dev@gmail.com>
2022-05-14 19:09:36 +0000

    refactoring: moved to services

--- inventory/app/controllers/api/v1/feeds_controller.rb
+++ inventory/app/controllers/api/v1/feeds_controller.rb
@@ -7,28 +7,13 @@ module Api
 
       def create
         data = feed_params
-        # Services::FeedsService::NewFeed.new(data[:store], data[:model], data[:inventory])
-        # Services::NewFeedService.new(1,1,1).call
-
-        puts data
-        
-        store = Store.find_or_create_by(name: data[:store])
-        shoe_model = ShoeModel.find_or_create_by(name: data[:model])
-        stock = Stock.create({store: store, shoe_model: shoe_model, item_count: data[:inventory]})
-        stock.save!
-
-        html_table = ApplicationController.render(
-          layout: false,
-          partial: 'stores/store_inventory',
-          locals: { store: store },
-        )
-        ActionCable.server.broadcast('feed_channel', {
-          html_table: html_table, 
-          below_lower_limit_count: store.stocks.last_records.below_lower_limit.size,
-          store_name: store.name.parameterize,
-          over_upper_limit_count: store.stocks.last_records.over_upper_limit.size
-        })
-        render json: stock
+        result = FeedServices::NewFeedService.new(
+          data[:store],
+          data[:model],
+          data[:inventory]
+        ).call
+        StoreServices::BroadcastStoreStock.new(result.store.id).call()
+        render json: :ok
       end
 
       private
