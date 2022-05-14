80e4b37b837b80e367ba86112482209c0513d9c4
amirali-ashraf <amiraliash.dev@gmail.com>
2022-05-14 21:35:24 +0000

    debugged and tested

--- inventory/app/services/stock_services/move_shoe_model.rb
+++ inventory/app/services/stock_services/move_shoe_model.rb
@@ -3,7 +3,7 @@ module StockServices
     def initialize(from_store_id, to_store_id, shoe_model_id, movement_count: 10)
       @from_store_id = from_store_id
       @to_store_id = to_store_id
-      @shoe_model_id = @shoe_model_id
+      @shoe_model_id = shoe_model_id
       @movement_count = movement_count
     end
 
@@ -11,7 +11,6 @@ module StockServices
       ActiveRecord::Base.transaction do
         from_stock = _from_stock_async(@from_store_id, @shoe_model_id)
         to_stock = _to_stock_async(@to_store_id, @shoe_model_id)
-        raise StandardError, "Stock not found" if from_stock.nil? or to_stock.nil?
         _from_stock_out(from_stock)
         _to_stock_in(to_stock)
       end
@@ -20,31 +19,35 @@ module StockServices
     private
 
     def _from_stock_async(from_store_id, shoe_model_id)
-      Stock.lock
+      from_stock = Stock.lock
       .where(store_id: from_store_id, shoe_model_id: shoe_model_id)
-      .where('item_count > ?', Stock::LOWER_LIMIT)
       .last
+      raise StandardError, "from_stock was not found." if from_stock.nil?
+      raise StandardError, "from_stock does not have enough items." if from_stock.item_count - @movement_count < Stock::LOWER_LIMIT
+      return from_stock
     end
 
     def _to_stock_async(to_store_id, shoe_model_id)
-      Stock.lock
+      to_stock = Stock.lock
       .where(store_id: to_store_id, shoe_model_id: shoe_model_id)
-      .where('item_count < ?', Stock::LOWER_LIMIT)
       .last
+      raise StandardError, "to_stock was not found." if to_stock.nil?
+      raise StandardError, "to_stock has enough items." if to_stock.item_count > Stock::UPPER_LIMIT
+      return to_stock
     end
 
     def _from_stock_out(from_stock)
       Stock.create(
-        store_id: from_stock.store_id, 
-        shoe_model_id: from_stock.shoe_model_id, 
+        store_id: from_stock.store_id,
+        shoe_model_id: from_stock.shoe_model_id,
         item_count: from_stock.item_count - @movement_count
       )
     end
 
     def _to_stock_in(to_stock)
       Stock.create(
-        store_id: to_stock.store_id, 
-        shoe_model_id: to_stock.shoe_model_id, 
+        store_id: to_stock.store_id,
+        shoe_model_id: to_stock.shoe_model_id,
         item_count: to_stock.item_count + @movement_count
       )
     end
