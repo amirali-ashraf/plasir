6611bbeb9632dc7e7302094d1288097ab5bb65da
amirali-ashraf <amiraliash.dev@gmail.com>
2022-05-14 19:10:31 +0000

    add new feeds to the database

--- inventory/app/services/feed_services/new_feed_service.rb
+++ inventory/app/services/feed_services/new_feed_service.rb
@@ -1,17 +1,36 @@
 
 module FeedServices
   class NewFeedService
-    def initialize(store_name, model_name, inventory_count)
+    def initialize(store_name, shoe_model_name, inventory_count)
       @store_name = store_name
-      @model_name = model_name
+      @shoe_model_name = shoe_model_name
       @inventory_count = inventory_count
     end
   
     def call
-      store = Store.find_or_create_by(name: @store_name)
-      shoe_model = ShoeModel.find_or_create_by(name: @model_name)
-      stock = Stock.find_or_create_by({store: store, shoe_model: shoe_model, count: @inventory_count})
-      # inventory = Stock.update({store: store, shoe_model: shoe_model, count: @inventory_count})
+      return ActiveRecord::Base.transaction do
+        store = _create_store
+        shoe_model = _create_shoe_model
+        stock = _create_stock(store, shoe_model)
+        OpenStruct.new(
+          store: store,
+          shoe_model: shoe_model,
+          stock: stock
+        )
+      end
+    end
+
+    private
+    def _create_store
+      Store.find_or_create_by(name: @store_name)
+    end
+
+    def _create_shoe_model
+      ShoeModel.find_or_create_by(name: @shoe_model_name)
+    end
+
+    def _create_stock(store, shoe_model)
+      Stock.create({store: store, shoe_model: shoe_model, item_count: @inventory_count})
     end
   end 
 end
