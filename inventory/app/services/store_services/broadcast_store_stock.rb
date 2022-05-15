91fb0d9d511e7878d04a83e61fc8e1469087fc1c
amirali-ashraf <amiraliash.dev@gmail.com>
2022-05-15 21:14:41 +0000

    trailing space is removed

--- inventory/app/services/store_services/broadcast_store_stock.rb
+++ inventory/app/services/store_services/broadcast_store_stock.rb
@@ -19,7 +19,7 @@ module StoreServices
 
     def _broadcast(html_table)
       ActionCable.server.broadcast('feed_channel', {
-        html_table: html_table, 
+        html_table: html_table,
         below_lower_limit_count: @store.stocks.last_records.below_lower_limit.size,
         store_name: @store.name.parameterize,
         over_upper_limit_count: @store.stocks.last_records.over_upper_limit.size
